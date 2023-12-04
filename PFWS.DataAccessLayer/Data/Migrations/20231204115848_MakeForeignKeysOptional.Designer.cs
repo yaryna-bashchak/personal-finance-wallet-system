cc0824aa6bcbeae3f1edf325a4886a015226a3d1
yaryna-bashchak <yaryna.bashchak@gmail.com>
2023-12-04 14:00:57 +0200

    make foreign keys in transaction optional

--- PFWS.DataAccessLayer/Data/Migrations/20231204115848_MakeForeignKeysOptional.Designer.cs
+++ PFWS.DataAccessLayer/Data/Migrations/20231204115848_MakeForeignKeysOptional.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PFWS.DataAccessLayer.Data;
 
@@ -10,9 +11,11 @@ using PFWS.DataAccessLayer.Data;
 namespace PFWS.DataAccessLayer.Data.Migrations
 {
     [DbContext(typeof(WalletContext))]
-    partial class WalletContextModelSnapshot : ModelSnapshot
+    [Migration("20231204115848_MakeForeignKeysOptional")]
+    partial class MakeForeignKeysOptional
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.14");
---
+++ PFWS.DataAccessLayer/Data/Migrations/20231204115848_MakeForeignKeysOptional.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PFWS.DataAccessLayer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeForeignKeysOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "10a71cfd-773c-4ca0-8f7f-8bf16e2f8b63");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cea5c6bc-90e1-4475-992d-de65fc228dc2");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ToAccountId",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IncomeCategoryId",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FromAccountId",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExpenseCategoryId",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ToAccountId",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IncomeCategoryId",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FromAccountId",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExpenseCategoryId",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "10a71cfd-773c-4ca0-8f7f-8bf16e2f8b63", null, "Admin", "ADMIN" },
+                    { "cea5c6bc-90e1-4475-992d-de65fc228dc2", null, "Member", "MEMBER" }
+                });
+        }
+    }
+}
