6a00bc40d2f4d6f25a9c6e35909bfb094f577308
yaryna-bashchak <yaryna.bashchak@gmail.com>
2023-12-11 21:38:05 +0200

    create initial projects for tests

---
+++ PFWS.API.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PFWS.API.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
