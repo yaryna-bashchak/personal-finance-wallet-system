2caa32effd936a7102c07d29f69d02e903667052
yaryna-bashchak <yaryna.bashchak@gmail.com>
2023-12-12 19:22:09 +0200

    add tests on AccountController when exeption were thrown

--- PFWS.API.Tests/AccountsControllerTests.cs
+++ PFWS.API.Tests/AccountsControllerTests.cs
@@ -12,13 +12,13 @@ public class AccountsControllerTests
 {
     private Mock<IAccountService> mockService;
     private AccountsController controller;
-    private string username = "testUser";
+    private readonly string username = "testUser";
 
     [SetUp]
     public void Setup()
     {
         mockService = new();
-        controller = new AccountsController(mockService.Object);
+        controller = new(mockService.Object);
         SetupControllerHttpContext(controller, username);
     }
 
@@ -89,6 +89,78 @@ public class AccountsControllerTests
         var result = await controller.DeleteAccount(1);
 
         Assert.IsInstanceOf<NoContentResult>(result);
+    }
+
+    [Test]
+    public async Task GetAccountById_WhenExceptionThrown_ReturnsBadRequest()
+    {
+        var exceptionMessage = "Error retrieving account";
+        mockService.Setup(s => s.GetAccountById(It.IsAny<int>(), It.IsAny<string>()))
+                   .ThrowsAsync(new Exception(exceptionMessage));
+
+        var result = await controller.GetAccountById(1);
+
+        Assert.IsInstanceOf<BadRequestObjectResult>(result.Result);
+        var badRequestResult = result.Result as BadRequestObjectResult;
+        Assert.That(badRequestResult?.Value, Is.EqualTo(exceptionMessage));
+    }
+
+    [Test]
+    public async Task GetUserAccounts_WhenExceptionThrown_ReturnsBadRequest()
+    {
+        var exceptionMessage = "Error retrieving accounts";
+        mockService.Setup(s => s.GetUserAccounts(It.IsAny<string>()))
+                   .ThrowsAsync(new Exception(exceptionMessage));
+
+        var result = await controller.GetUserAccounts();
+
+        Assert.IsInstanceOf<BadRequestObjectResult>(result.Result);
+        var badRequestResult = result.Result as BadRequestObjectResult;
+        Assert.That(badRequestResult?.Value, Is.EqualTo(exceptionMessage));
+    }
+
+    [Test]
+    public async Task AddAccount_WhenExceptionThrown_ReturnsBadRequest()
+    {
+        var newAccount = new AddAccountDto { Balance = 1000 };
+        var exceptionMessage = "Error adding account";
+        mockService.Setup(s => s.AddAccount(It.IsAny<AddAccountDto>(), It.IsAny<string>()))
+                   .ThrowsAsync(new Exception(exceptionMessage));
+
+        var result = await controller.AddAccount(newAccount);
+
+        Assert.IsInstanceOf<BadRequestObjectResult>(result);
+        var badRequestResult = result as BadRequestObjectResult;
+        Assert.That(badRequestResult?.Value, Is.EqualTo(exceptionMessage));
+    }
+
+    [Test]
+    public async Task UpdateAccount_WhenExceptionThrown_ReturnsBadRequest()
+    {
+        var updatedAccount = new UpdateAccountDto { Name = "Updated account name"};
+        var exceptionMessage = "Error updating account";
+        mockService.Setup(s => s.UpdateAccount(It.IsAny<int>(), It.IsAny<UpdateAccountDto>(), It.IsAny<string>()))
+                   .ThrowsAsync(new Exception(exceptionMessage));
+
+        var result = await controller.UpdateAccount(1, updatedAccount);
+
+        Assert.IsInstanceOf<BadRequestObjectResult>(result);
+        var badRequestResult = result as BadRequestObjectResult;
+        Assert.That(badRequestResult?.Value, Is.EqualTo(exceptionMessage));
+    }
+
+    [Test]
+    public async Task DeleteAccount_WhenExceptionThrown_ReturnsBadRequest()
+    {
+        var exceptionMessage = "Error deleting account";
+        mockService.Setup(s => s.DeleteAccount(It.IsAny<int>(), It.IsAny<string>()))
+                   .ThrowsAsync(new Exception(exceptionMessage));
+
+        var result = await controller.DeleteAccount(1);
+
+        Assert.IsInstanceOf<BadRequestObjectResult>(result);
+        var badRequestResult = result as BadRequestObjectResult;
+        Assert.That(badRequestResult?.Value, Is.EqualTo(exceptionMessage));
     }
 
     private static void SetupControllerHttpContext(ControllerBase controller, string username)
